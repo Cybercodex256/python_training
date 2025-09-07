bfd2111a204a07c2be44f878431841153bb920f8
cybercodex256 <kamugishaedwin256@gmail.com>
2025-09-07 07:52:56 +0300

    Successfully created repo for python exercises

--- timer.py
+++ timer.py
@@ -1,11 +1,11 @@
 import time
-from os import system
+import os
 
 def countDown(t):
  while t>0:
     print(f"Time remaining: {t} seconds")
     time.sleep(1)
-    system("clear")
+    os.system("clear")
     t-=1
     
  print("Time is out")
